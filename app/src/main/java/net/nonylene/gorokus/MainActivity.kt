f3935cd7ff84010e48c735f1e9efd6b92eef5316
nonylene <cookie.speaker@gmail.com>
2016-05-30 00:03:58 +0900

    fix and set example gorokus

--- app/src/main/java/net/nonylene/gorokus/MainActivity.kt
+++ app/src/main/java/net/nonylene/gorokus/MainActivity.kt
@@ -16,6 +16,7 @@ import net.nonylene.gorokus.databinding.RecyclerItemCategoryBinding
 import net.nonylene.gorokus.databinding.RecyclerItemGorokuBinding
 import net.nonylene.gorokus.model.Category
 import net.nonylene.gorokus.model.Goroku
+import net.nonylene.gorokus.model.newCategoryId
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,6 +35,21 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        realm.executeTransactionAsync(Realm.Transaction { realm ->
+            val category = realm.createObject(Category::class.java)
+            category.id = newCategoryId(realm)
+            category.name = "Hoge"
+            val category2 = realm.createObject(Category::class.java)
+            category2.id = newCategoryId(realm)
+            category2.name = "HogePiyo"
+            val category3 = realm.createObject(Category::class.java)
+            category3.id = newCategoryId(realm)
+            category3.name = "HogePoPoPoe"
+        }, Realm.Transaction.OnSuccess {
+            adapter.categoryList = realm.where(Category::class.java).findAll()
+            adapter.notifyDataSetChanged()
+        })
+
         adapter.setHasStableIds(true)
         with(binding.recyclerView) {
             adapter = this@MainActivity.adapter
@@ -50,7 +66,7 @@ class MainActivity : AppCompatActivity() {
         var categoryList: List<Category> = listOf()
         var gorokuList: List<Goroku> = listOf()
 
-        override fun getItemCount() = gorokuList.size
+        override fun getItemCount() = categoryList.size + gorokuList.size
 
         override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
             val posInList = getPositionInList(position)
